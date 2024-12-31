39bfb64815d8c4b9c4fa795a843fe1bb3fe5f9f7
Jennifer <jennifer64reece@gmail.com>
2024-12-30 20:34:32 -0800

    adding addEventListener

--- index.js
+++ index.js
@@ -1,29 +1,36 @@
-// https://www.omdbapi.com/?apikey=3adaf1af&s=fast
+// https://www.omdbapi.com/?apikey=980b547b&s=fast
 
-async function main() {
-  const movies = await fetch('https://www.omdbapi.com/?apikey=3adaf1af&s=fast')
-  const moviesData = await movies.json()
+async function main(filter) {
+  const movies = await fetch('https://www.omdbapi.com/?apikey=980b547b&s=fast');
+  const moviesData = await movies.json();
   const moviesEl = document.querySelector('.movies')
-  moviesEl.innerHTML = moviesData.Search.map(movie => movieHTML(movie)).join('')
-}
+  const searchInput = document.getElementsByClassName('search-bar--input')
+  const dropdown = document.getElementById('dropdown')
 
-main()
+  searchInput.addEventListener('input', () => {
+    
+})
 
-function showMovie(movie) {
-  console.log(movie)
-}
-
-function movieHTML(movie) {
-  return `<div class="movie" onclick="showMovie(${movie.Poster})">
+  console.log(moviesData.Search)
+  moviesEl.innerHTML = moviesData.Search
+    .map(
+      (movie) => `<div class="movie">
     <div class="movie__wrapper">
       <img class="movie__img" src="${movie.Poster}" alt="">
       <div class="movie__description">
-        <div class="movie__title">${movie.title}</div>
+        <div class="movie__title">${movie.Title}</div>
         <div class="space-between">
-          <div class="movie__year">${movie.year}</div>
+          <div class="movie__year">${movie.Year}</div>
           <div class="imdbID">${movie.imdbID}</div>
         </div>
       </div>
     </div>
   </div>`
+    ).join('')
 }
+
+
+
+main();
+
+
